22822ea555f25d6c068ebabe666695cab8fcc32f
Tomaž Karer <tomaz@karer.si>
2021-09-24 16:57:31 +0200

    delo

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.bash_profile ~/.bash_profile
 
 mkdir ~/.config/nvim/lua -p
-ln -s -f $(pwd)/neovim/init.lua ~/.config/nvim/init.lua
+ln -s -f $(pwd)/neovim/init.vim ~/.config/nvim/init.vim
 ln -s -f $(pwd)/neovim/lua/settings.lua ~/.config/nvim/lua/settings.lua
+ln -s -f $(pwd)/neovim/lua/init.lua ~/.config/nvim/lua/init.lua
 ln -s -f $(pwd)/neovim/lua/plugins.lua ~/.config/nvim/lua/plugins.lua
 
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -7,21 +7,42 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd 'packadd packer.nvim'
 end
 
-api.nvim_set_keymap('n', '<leader>ff', ":Telescope find_files<cr>", { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<leader>fg', ":Telescope live_grep<cr>", { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<leader>fb', ":Telescope buffers<cr>", { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<leader>fh', ":Telescope help_tags<cr>", { noremap = true, silent = true })
+--api.nvim_set_keymap('n', '<leader>ff', ":Telescope find_files<cr>", { noremap = true, silent = true })
+--api.nvim_set_keymap('n', '<leader>fg', ":Telescope live_grep<cr>", { noremap = true, silent = true })
+--api.nvim_set_keymap('n', '<leader>fb', ":Telescope buffers<cr>", { noremap = true, silent = true })
+--api.nvim_set_keymap('n', '<leader>fh', ":Telescope help_tags<cr>", { noremap = true, silent = true })\\
+
+--lspconfig
+local nvim_lsp = require("lspconfig")
+
+--fzf
+api.nvim_set_keymap('n', '<leader>ff', ":Files<cr>", { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<leader>fg', ":Rg<cr>", { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<leader>fb', ":Buffers<cr>", { noremap = true, silent = true })
+
+-- vimwiki
+vim.g.vimwiki_list = {{path = '~/development/personal/personal_wiki/', syntax = 'markdown', ext = '.md'}}
 
 require("packer").startup(
   function()
     use "wbthomason/packer.nvim"
     use {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
+    }
+    use {'junegunn/fzf', dir = '~/.fzf', run = ':call fzf#install()'}
+    use {'junegunn/fzf.vim'}
+
+    use {
+      'vimwiki/vimwiki'
+    }
+
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons'
+    }
+    use {
+      'neovim/nvim-lspconfig'
     }
   end
 )
